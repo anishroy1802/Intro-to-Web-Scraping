fa830256c080544e23426762db1ff8ba33441eca
anishroy1802 <73432372+anishroy1802@users.noreply.github.com>
2021-02-27 00:19:48 +0530

    Update moodleLogin.py

--- moodleLogin.py
+++ moodleLogin.py
@@ -7,8 +7,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
-PATH= "C:\Program Files\chromedriver.exe"
-driver= webdriver.Chrome(PATH)
+
+driver= webdriver.Chrome()
 
 driver.get("https://moodle.iitd.ac.in/login/index.php")
 texit = driver.find_element_by_id("login").text
